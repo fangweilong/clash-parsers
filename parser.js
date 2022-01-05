640070ac1b3e00078ac37eea3b903c230f03bd28
fangweilong <fangweilong@outlook.com>
2022-01-05 18:00:12 +0800

    添加代码，

---
+++ parser.js
@@ -0,0 +1,517 @@
+let areas =[
+{"flag":"🇦🇨","name":"阿森松岛"},
+{"flag":"🇦🇩","name":"安道尔"},
+{"flag":"🇦🇪","name":"阿拉伯联合酋长国"},
+{"flag":"🇦🇫","name":"阿富汗"},
+{"flag":"🇦🇬","name":"安提瓜和巴布达"},
+{"flag":"🇦🇮","name":"安圭拉"},
+{"flag":"🇦🇱","name":"阿尔巴尼亚"},
+{"flag":"🇦🇲","name":"亚美尼亚"},
+{"flag":"🇦🇴","name":"安哥拉"},
+{"flag":"🇦🇶","name":"南极洲"},
+{"flag":"🇦🇷","name":"阿根廷"},
+{"flag":"🇦🇸","name":"美属萨摩亚"},
+{"flag":"🇦🇹","name":"奥地利"},
+{"flag":"🇦🇺","name":"澳大利亚"},
+{"flag":"🇦🇼","name":"阿鲁巴"},
+{"flag":"🇦🇽","name":"奥兰群岛"},
+{"flag":"🇦🇿","name":"阿塞拜疆"},
+{"flag":"🇧🇦","name":"波斯尼亚和黑塞哥维那"},
+{"flag":"🇧🇧","name":"巴巴多斯"},
+{"flag":"🇧🇩","name":"孟加拉国"},
+{"flag":"🇧🇪","name":"比利时"},
+{"flag":"🇧🇫","name":"布基纳法索"},
+{"flag":"🇧🇬","name":"保加利亚"},
+{"flag":"🇧🇭","name":"巴林"},
+{"flag":"🇧🇮","name":"布隆迪"},
+{"flag":"🇧🇯","name":"贝宁"},
+{"flag":"🇧🇱","name":"圣巴泰勒米"},
+{"flag":"🇧🇲","name":"百慕大"},
+{"flag":"🇧🇳","name":"文莱"},
+{"flag":"🇧🇴","name":"玻利维亚"},
+{"flag":"🇧🇶","name":"荷属加勒比区"},
+{"flag":"🇧🇷","name":"巴西"},
+{"flag":"🇧🇸","name":"巴哈马"},
+{"flag":"🇧🇹","name":"不丹"},
+{"flag":"🇧🇻","name":"布韦岛"},
+{"flag":"🇧🇼","name":"博茨瓦纳"},
+{"flag":"🇧🇾","name":"白俄罗斯"},
+{"flag":"🇧🇿","name":"伯利兹"},
+{"flag":"🇨🇦","name":"加拿大"},
+{"flag":"🇨🇨","name":"科科斯（基林）群岛"},
+{"flag":"🇨🇩","name":"刚果（金）"},
+{"flag":"🇨🇫","name":"中非共和国"},
+{"flag":"🇨🇬","name":"刚果（布）"},
+{"flag":"🇨🇭","name":"瑞士"},
+{"flag":"🇨🇮","name":"科特迪瓦"},
+{"flag":"🇨🇰","name":"库克群岛"},
+{"flag":"🇨🇱","name":"智利"},
+{"flag":"🇨🇲","name":"喀麦隆"},
+{"flag":"🇨🇳","name":"中国"},
+{"flag":"🇨🇴","name":"哥伦比亚"},
+{"flag":"🇨🇵","name":"克利珀顿岛"},
+{"flag":"🇨🇷","name":"哥斯达黎加"},
+{"flag":"🇨🇺","name":"古巴"},
+{"flag":"🇨🇻","name":"佛得角"},
+{"flag":"🇨🇼","name":"库拉索"},
+{"flag":"🇨🇽","name":"圣诞岛"},
+{"flag":"🇨🇾","name":"塞浦路斯"},
+{"flag":"🇨🇿","name":"捷克"},
+{"flag":"🇩🇪","name":"德国"},
+{"flag":"🇩🇬","name":"迪戈加西亚岛"},
+{"flag":"🇩🇯","name":"吉布提"},
+{"flag":"🇩🇰","name":"丹麦"},
+{"flag":"🇩🇲","name":"多米尼克"},
+{"flag":"🇩🇴","name":"多米尼加共和国"},
+{"flag":"🇩🇿","name":"阿尔及利亚"},
+{"flag":"🇪🇦","name":"休达及梅利利亚"},
+{"flag":"🇪🇨","name":"厄瓜多尔"},
+{"flag":"🇪🇪","name":"爱沙尼亚"},
+{"flag":"🇪🇬","name":"埃及"},
+{"flag":"🇪🇭","name":"西撒哈拉"},
+{"flag":"🇪🇷","name":"厄立特里亚"},
+{"flag":"🇪🇸","name":"西班牙"},
+{"flag":"🇪🇹","name":"埃塞俄比亚"},
+{"flag":"🇪🇺","name":"欧盟"},
+{"flag":"🇫🇮","name":"芬兰"},
+{"flag":"🇫🇯","name":"斐济"},
+{"flag":"🇫🇰","name":"福克兰群岛"},
+{"flag":"🇫🇲","name":"密克罗尼西亚"},
+{"flag":"🇫🇴","name":"法罗群岛"},
+{"flag":"🇫🇷","name":"法国"},
+{"flag":"🇬🇦","name":"加蓬"},
+{"flag":"🇬🇧","name":"英国"},
+{"flag":"🇬🇩","name":"格林纳达"},
+{"flag":"🇬🇪","name":"格鲁吉亚"},
+{"flag":"🇬🇫","name":"法属圭亚那"},
+{"flag":"🇬🇬","name":"根西岛"},
+{"flag":"🇬🇭","name":"加纳"},
+{"flag":"🇬🇮","name":"直布罗陀"},
+{"flag":"🇬🇱","name":"格陵兰"},
+{"flag":"🇬🇲","name":"冈比亚"},
+{"flag":"🇬🇳","name":"几内亚"},
+{"flag":"🇬🇵","name":"瓜德罗普"},
+{"flag":"🇬🇶","name":"赤道几内亚"},
+{"flag":"🇬🇷","name":"希腊"},
+{"flag":"🇬🇸","name":"南乔治亚和南桑威奇群岛"},
+{"flag":"🇬🇹","name":"危地马拉"},
+{"flag":"🇬🇺","name":"关岛"},
+{"flag":"🇬🇼","name":"几内亚比绍"},
+{"flag":"🇬🇾","name":"圭亚那"},
+{"flag":"🇭🇰","name":"香港"},
+{"flag":"🇭🇲","name":"赫德岛和麦克唐纳群岛"},
+{"flag":"🇭🇳","name":"洪都拉斯"},
+{"flag":"🇭🇷","name":"克罗地亚"},
+{"flag":"🇭🇹","name":"海地"},
+{"flag":"🇭🇺","name":"匈牙利"},
+{"flag":"🇮🇨","name":"加纳利群岛"},
+{"flag":"🇮🇩","name":"印度尼西亚"},
+{"flag":"🇮🇪","name":"爱尔兰"},
+{"flag":"🇮🇱","name":"以色列"},
+{"flag":"🇮🇲","name":"马恩岛"},
+{"flag":"🇮🇳","name":"印度"},
+{"flag":"🇮🇴","name":"英属印度洋领地"},
+{"flag":"🇮🇶","name":"伊拉克"},
+{"flag":"🇮🇷","name":"伊朗"},
+{"flag":"🇮🇸","name":"冰岛"},
+{"flag":"🇮🇹","name":"意大利"},
+{"flag":"🇯🇪","name":"泽西岛"},
+{"flag":"🇯🇲","name":"牙买加"},
+{"flag":"🇯🇴","name":"约旦"},
+{"flag":"🇯🇵","name":"日本"},
+{"flag":"🇰🇪","name":"肯尼亚"},
+{"flag":"🇰🇬","name":"吉尔吉斯斯坦"},
+{"flag":"🇰🇭","name":"柬埔寨"},
+{"flag":"🇰🇮","name":"基里巴斯"},
+{"flag":"🇰🇲","name":"科摩罗"},
+{"flag":"🇰🇳","name":"圣基茨和尼维斯"},
+{"flag":"🇰🇵","name":"朝鲜"},
+{"flag":"🇰🇷","name":"韩国"},
+{"flag":"🇰🇼","name":"科威特"},
+{"flag":"🇰🇾","name":"开曼群岛"},
+{"flag":"🇰🇿","name":"哈萨克斯坦"},
+{"flag":"🇱🇦","name":"老挝"},
+{"flag":"🇱🇧","name":"黎巴嫩"},
+{"flag":"🇱🇨","name":"圣卢西亚"},
+{"flag":"🇱🇮","name":"列支敦士登"},
+{"flag":"🇱🇰","name":"斯里兰卡"},
+{"flag":"🇱🇷","name":"利比里亚"},
+{"flag":"🇱🇸","name":"莱索托"},
+{"flag":"🇱🇹","name":"立陶宛"},
+{"flag":"🇱🇺","name":"卢森堡"},
+{"flag":"🇱🇻","name":"拉脱维亚"},
+{"flag":"🇱🇾","name":"利比亚"},
+{"flag":"🇲🇦","name":"摩洛哥"},
+{"flag":"🇲🇨","name":"摩纳哥"},
+{"flag":"🇲🇩","name":"摩尔多瓦"},
+{"flag":"🇲🇪","name":"黑山"},
+{"flag":"🇲🇫","name":"法属圣马丁"},
+{"flag":"🇲🇬","name":"马达加斯加"},
+{"flag":"🇲🇭","name":"马绍尔群岛"},
+{"flag":"🇲🇰","name":"北马其顿"},
+{"flag":"🇲🇱","name":"马里"},
+{"flag":"🇲🇲","name":"缅甸"},
+{"flag":"🇲🇳","name":"蒙古"},
+{"flag":"🇲🇴","name":"澳门"},
+{"flag":"🇲🇵","name":"北马里亚纳群岛"},
+{"flag":"🇲🇶","name":"马提尼克"},
+{"flag":"🇲🇷","name":"毛里塔尼亚"},
+{"flag":"🇲🇸","name":"蒙特塞拉特"},
+{"flag":"🇲🇹","name":"马耳他"},
+{"flag":"🇲🇺","name":"毛里求斯"},
+{"flag":"🇲🇻","name":"马尔代夫"},
+{"flag":"🇲🇼","name":"马拉维"},
+{"flag":"🇲🇽","name":"墨西哥"},
+{"flag":"🇲🇾","name":"马来西亚"},
+{"flag":"🇲🇿","name":"莫桑比克"},
+{"flag":"🇳🇦","name":"纳米比亚"},
+{"flag":"🇳🇨","name":"新喀里多尼亚"},
+{"flag":"🇳🇪","name":"尼日尔"},
+{"flag":"🇳🇫","name":"诺福克岛"},
+{"flag":"🇳🇬","name":"尼日利亚"},
+{"flag":"🇳🇮","name":"尼加拉瓜"},
+{"flag":"🇳🇱","name":"荷兰"},
+{"flag":"🇳🇴","name":"挪威"},
+{"flag":"🇳🇵","name":"尼泊尔"},
+{"flag":"🇳🇷","name":"瑙鲁"},
+{"flag":"🇳🇺","name":"纽埃"},
+{"flag":"🇳🇿","name":"新西兰"},
+{"flag":"🇴🇲","name":"阿曼"},
+{"flag":"🇵🇦","name":"巴拿马"},
+{"flag":"🇵🇪","name":"秘鲁"},
+{"flag":"🇵🇫","name":"法属波利尼西亚"},
+{"flag":"🇵🇬","name":"巴布亚新几内亚"},
+{"flag":"🇵🇭","name":"菲律宾"},
+{"flag":"🇵🇰","name":"巴基斯坦"},
+{"flag":"🇵🇱","name":"波兰"},
+{"flag":"🇵🇲","name":"圣皮埃尔和密克隆群岛"},
+{"flag":"🇵🇳","name":"皮特凯恩群岛"},
+{"flag":"🇵🇷","name":"波多黎各"},
+{"flag":"🇵🇸","name":"巴勒斯坦领土"},
+{"flag":"🇵🇹","name":"葡萄牙"},
+{"flag":"🇵🇼","name":"帕劳"},
+{"flag":"🇵🇾","name":"巴拉圭"},
+{"flag":"🇶🇦","name":"卡塔尔"},
+{"flag":"🇷🇪","name":"留尼汪"},
+{"flag":"🇷🇴","name":"罗马尼亚"},
+{"flag":"🇷🇸","name":"塞尔维亚"},
+{"flag":"🇷🇺","name":"俄罗斯"},
+{"flag":"🇷🇼","name":"卢旺达"},
+{"flag":"🇸🇦","name":"沙特阿拉伯"},
+{"flag":"🇸🇧","name":"所罗门群岛"},
+{"flag":"🇸🇨","name":"塞舌尔"},
+{"flag":"🇸🇩","name":"苏丹"},
+{"flag":"🇸🇪","name":"瑞典"},
+{"flag":"🇸🇬","name":"新加坡"},
+{"flag":"🇸🇭","name":"圣赫勒拿"},
+{"flag":"🇸🇮","name":"斯洛文尼亚"},
+{"flag":"🇸🇯","name":"斯瓦尔巴和扬马延"},
+{"flag":"🇸🇰","name":"斯洛伐克"},
+{"flag":"🇸🇱","name":"塞拉利昂"},
+{"flag":"🇸🇲","name":"圣马力诺"},
+{"flag":"🇸🇳","name":"塞内加尔"},
+{"flag":"🇸🇴","name":"索马里"},
+{"flag":"🇸🇷","name":"苏里南"},
+{"flag":"🇸🇸","name":"南苏丹"},
+{"flag":"🇸🇹","name":"圣多美和普林西比"},
+{"flag":"🇸🇻","name":"萨尔瓦多"},
+{"flag":"🇸🇽","name":"荷属圣马丁"},
+{"flag":"🇸🇾","name":"叙利亚"},
+{"flag":"🇸🇿","name":"斯威士兰"},
+{"flag":"🇹🇦","name":"特里斯坦-达库尼亚群岛"},
+{"flag":"🇹🇨","name":"特克斯和凯科斯群岛"},
+{"flag":"🇹🇩","name":"乍得"},
+{"flag":"🇹🇫","name":"法属南部领地"},
+{"flag":"🇹🇬","name":"多哥"},
+{"flag":"🇹🇭","name":"泰国"},
+{"flag":"🇹🇯","name":"塔吉克斯坦"},
+{"flag":"🇹🇰","name":"托克劳"},
+{"flag":"🇹🇱","name":"东帝汶"},
+{"flag":"🇹🇲","name":"土库曼斯坦"},
+{"flag":"🇹🇳","name":"突尼斯"},
+{"flag":"🇹🇴","name":"汤加"},
+{"flag":"🇹🇷","name":"土耳其"},
+{"flag":"🇹🇹","name":"特立尼达和多巴哥"},
+{"flag":"🇹🇻","name":"图瓦卢"},
+{"flag":"🇹🇼","name":"台湾"},
+{"flag":"🇹🇿","name":"坦桑尼亚"},
+{"flag":"🇺🇦","name":"乌克兰"},
+{"flag":"🇺🇬","name":"乌干达"},
+{"flag":"🇺🇲","name":"美国本土外小岛屿"},
+{"flag":"🇺🇳","name":"联合国"},
+{"flag":"🇺🇸","name":"美国"},
+{"flag":"🇺🇾","name":"乌拉圭"},
+{"flag":"🇺🇿","name":"乌兹别克斯坦"},
+{"flag":"🇻🇦","name":"梵蒂冈"},
+{"flag":"🇻🇨","name":"圣文森特和格林纳丁斯"},
+{"flag":"🇻🇪","name":"委内瑞拉"},
+{"flag":"🇻🇬","name":"英属维尔京群岛"},
+{"flag":"🇻🇮","name":"美属维尔京群岛"},
+{"flag":"🇻🇳","name":"越南"},
+{"flag":"🇻🇺","name":"瓦努阿图"},
+{"flag":"🇼🇫","name":"瓦利斯和富图纳"},
+{"flag":"🇼🇸","name":"萨摩亚"},
+{"flag":"🇽🇰","name":"科索沃"},
+{"flag":"🇾🇪","name":"也门"},
+{"flag":"🇾🇹","name":"马约特"},
+{"flag":"🇿🇦","name":"南非"},
+{"flag":"🇿🇲","name":"赞比亚"},
+{"flag":"🇿🇼","name":"津巴布韦 "}]
+
+//路由规则
+let rule_providers=`
+rule-providers:
+    reject:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/reject.txt
+      path: ./ruleset/reject.yaml
+      interval: 86400
+    icloud:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/icloud.txt
+      path: ./ruleset/icloud.yaml
+      interval: 86400
+    apple:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/apple.txt
+      path: ./ruleset/apple.yaml
+      interval: 86400
+    google:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/google.txt
+      path: ./ruleset/google.yaml
+      interval: 86400
+    proxy:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/proxy.txt
+      path: ./ruleset/proxy.yaml
+      interval: 86400
+    direct:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/direct.txt
+      path: ./ruleset/direct.yaml
+      interval: 86400
+    private:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/private.txt
+      path: ./ruleset/private.yaml
+      interval: 86400
+    gfw:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/gfw.txt
+      path: ./ruleset/gfw.yaml
+      interval: 86400
+    greatfire:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/greatfire.txt
+      path: ./ruleset/greatfire.yaml
+      interval: 86400
+    tld-not-cn:
+      type: http
+      behavior: domain
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/tld-not-cn.txt
+      path: ./ruleset/tld-not-cn.yaml
+      interval: 86400
+    telegramcidr:
+      type: http
+      behavior: ipcidr
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/telegramcidr.txt
+      path: ./ruleset/telegramcidr.yaml
+      interval: 86400
+    cncidr:
+      type: http
+      behavior: ipcidr
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/cncidr.txt
+      path: ./ruleset/cncidr.yaml
+      interval: 86400
+    lancidr:
+      type: http
+      behavior: ipcidr
+      url: https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/lancidr.txt
+      path: ./ruleset/lancidr.yaml
+      interval: 86400`
+
+/**
+ * rules规则
+ *
+ * 匹配规则,域名或者关键字等,类型
+ *
+ * {@link https://docs.cfw.lbyczf.com/contents/ui/profiles/rules.html#%E8%A7%84%E5%88%99%E7%BC%96%E8%BE%91}
+ */
+let rules=[
+    "PROCESS-NAME,v2ray,DIRECT",
+    "PROCESS-NAME,xray,DIRECT",
+    "PROCESS-NAME,naive,DIRECT",
+    "PROCESS-NAME,trojan,DIRECT",
+    "PROCESS-NAME,trojan-go,DIRECT",
+    "PROCESS-NAME,ss-local,DIRECT",
+    "PROCESS-NAME,privoxy,DIRECT",
+    "PROCESS-NAME,leaf,DIRECT",
+    "PROCESS-NAME,v2ray.exe,DIRECT",
+    "PROCESS-NAME,xray.exe,DIRECT",
+    "PROCESS-NAME,naive.exe,DIRECT",
+    "PROCESS-NAME,trojan.exe,DIRECT",
+    "PROCESS-NAME,trojan-go.exe,DIRECT",
+    "PROCESS-NAME,ss-local.exe,DIRECT",
+    "PROCESS-NAME,privoxy.exe,DIRECT",
+    "PROCESS-NAME,leaf.exe,DIRECT",
+    "PROCESS-NAME,Surge,DIRECT",
+    "PROCESS-NAME,Surge 2,DIRECT",
+    "PROCESS-NAME,Surge 3,DIRECT",
+    "PROCESS-NAME,Surge 4,DIRECT",
+    "PROCESS-NAME,Surge%202,DIRECT",
+    "PROCESS-NAME,Surge%203,DIRECT",
+    "PROCESS-NAME,Surge%204,DIRECT",
+    "PROCESS-NAME,Thunder,DIRECT",
+    "PROCESS-NAME,DownloadService,DIRECT",
+    "PROCESS-NAME,qBittorrent,DIRECT",
+    "PROCESS-NAME,Transmission,DIRECT",
+    "PROCESS-NAME,fdm,DIRECT",
+    "PROCESS-NAME,aria2c,DIRECT",
+    "PROCESS-NAME,Folx,DIRECT",
+    "PROCESS-NAME,NetTransport,DIRECT",
+    "PROCESS-NAME,uTorrent,DIRECT",
+    "PROCESS-NAME,WebTorrent,DIRECT",
+    "PROCESS-NAME,aria2c.exe,DIRECT",
+    "PROCESS-NAME,BitComet.exe,DIRECT",
+    "PROCESS-NAME,fdm.exe,DIRECT",
+    "PROCESS-NAME,NetTransport.exe,DIRECT",
+    "PROCESS-NAME,qbittorrent.exe,DIRECT",
+    "PROCESS-NAME,Thunder.exe,DIRECT",
+    "PROCESS-NAME,ThunderVIP.exe,DIRECT",
+    "PROCESS-NAME,transmission-daemon.exe,DIRECT",
+    "PROCESS-NAME,transmission-qt.exe,DIRECT",
+    "PROCESS-NAME,uTorrent.exe,DIRECT",
+    "PROCESS-NAME,WebTorrent.exe,DIRECT",
+    "DOMAIN,clash.razord.top,DIRECT",
+    "DOMAIN,yacd.haishan.me,DIRECT",
+    "RULE-SET,private,DIRECT",
+    "RULE-SET,direct,DIRECT",
+    "RULE-SET,icloud,DIRECT",
+    "RULE-SET,apple,DIRECT",
+    "RULE-SET,google,DIRECT",
+    "RULE-SET,reject,🛑 全球拦截",
+    "GEOIP,CN,🌏 全球直连",
+    "RULE-SET,proxy,♻️ 自动选择",
+    "RULE-SET,telegramcidr,♻️ 自动选择",
+    "MATCH,DIRECT"
+]
+
+//切换时间 12小时
+let intervalTime = 43200
+
+//自动选择
+let automatic={
+    "name": "♻️ 自动选择",
+    "type": "url-test",
+    "url": "http://www.gstatic.com/generate_204",
+    "interval": intervalTime,
+    "tolerance": 500,//切换阈值
+    "proxies":[]
+}
+//选择节点
+let select={
+    "name": "✅ 选择节点",
+    "type": "select",
+    "proxies":["♻️ 自动选择"]
+}
+//故障转移
+let fallback={
+    "name": "🔯 故障转移",
+    "type": "fallback",
+    "url": "http://www.gstatic.com/generate_204",
+    "interval": 300,
+    "proxies":[]
+}
+//直连
+let direct={
+    "name": "🌏 全球直连",
+    "type": "select",
+    "proxies":["DIRECT","✅ 选择节点","♻️ 自动选择"]
+}
+//阻止链接
+let prevent={
+    "name": "🛑 全球拦截",
+    "type": "select",
+    "proxies":["REJECT", "DIRECT"]
+}
+
+let all={
+    "name": "🈷️ 所有节点",
+    "type": "select",
+    "proxies":[]
+}
+
+
+
+
+
+module.exports.parse = async function(raw, {axios, yaml, notify,console},{ name, url, interval, selected }) {
+    let content = yaml.parse(raw);
+    //置空proxy-groups,添加自己的规则
+    content['proxy-groups']=[]
+    content["rules"]=rules
+
+    //遍历地区来分组
+    for (let area of areas){
+        let areaJson={},
+        proxies = [],
+        regionName=area.flag+" "+area.name ;
+        for (let proxy of content.proxies) {
+            if (proxy.server === undefined) break;
+            //如果匹配上了就加入
+            if (proxy.name.indexOf(area.name) !== -1) {
+                if(areaJson["name"] === regionName){
+                    proxies.push(proxy.name);
+                }else{
+                    proxies.push(proxy.name);
+                    areaJson["name"]=regionName;
+                }
+            }else{
+                // console.log("匹配不成功",proxy.name,area.name)
+            }
+            //添加所有的节点到一个分组中，方便测试连通
+            all["proxies"].push(proxy.name)
+        }
+        if(areaJson["name"]){
+            // areaJson["type"]= "url-test";//类型为自动测试。其他如负载均衡等
+            // areaJson["url"]="https://www.gstatic.com/generate_204";//测试url
+            areaJson["type"]="select"
+            areaJson["proxies"]=proxies;
+            areaJson["interval"] = intervalTime;//更新周期
+            //放到yml中
+            content['proxy-groups'].push(areaJson)
+            //对几个预置的规则进行处理
+            select["proxies"].push(regionName)
+            fallback["proxies"].push(regionName)
+            automatic["proxies"].push(regionName)
+        }
+    }
+
+    /**
+     * 添加各种分组。
+     *
+     * {@link https://github.com/Loyalsoldier/clash-rules}
+     */
+    content['proxy-groups'].splice(0,0,all);//阻止链接
+    content['proxy-groups'].splice(0,0,prevent);//阻止链接
+    content['proxy-groups'].splice(0,0,direct);//直连
+    content['proxy-groups'].splice(0,0,fallback);//故障
+    content['proxy-groups'].splice(0,0,select);//手动选择
+    content['proxy-groups'].splice(0,0,automatic);//自动选择
+
+
+    let returnStr = yaml.stringify(content)+rule_providers;
+
+    console.log(returnStr)
+    return returnStr;
+  }
